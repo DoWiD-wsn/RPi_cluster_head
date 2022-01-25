c3d9831252dc3018b7596a78056c4da120d86ab7
Dominik Widhalm <widhalm@technikum-wien.at>
2022-01-25 13:33:53 +0100

    Added CH script for ftDCA New database structure, new message structure Receive danger and safe indicator from nodes Perform DCA cell update on CH

--- source/rpi_cluster_head.py
+++ source/rpi_cluster_head.py
@@ -9,12 +9,12 @@
 # data are then stored in a remote database.
 # Additionally, specified information is written into a log file.
 #
-# This is the version used in the centralized DCA WSN testbed.
+# Version is used in the temporospatial-fault-DCA (tfDCA) WSN testbed.
 #
 # @file     rpi_cluster_head.py
 # @author   Dominik Widhalm
-# @version  0.1.3
-# @date     2021/12/07
+# @version  0.3.0
+# @date     2022/01/25
 #####
 
 
@@ -46,6 +46,10 @@ from mysql.connector import errorcode
 # Import the Xbee library
 from digi.xbee.devices import XBeeDevice, RemoteXBeeDevice
 from digi.xbee.models.address import XBee64BitAddress
+# Parser for the configuration file
+import configparser
+parser = configparser.ConfigParser()
+parser.read("config.conf")
 
 
 ##### GLOBAL VARIABLES #####
@@ -61,18 +65,18 @@ SAFE_SENS       = 0.1
 XBEE_SERIAL_DEV     = "/dev/ttyUSB0"
 
 # database connection details (adapt if necessary!)
-DB_CON_HOST         = "xxx.xxx.xxx.xxx"
-DB_CON_USER         = "USER"
-DB_CON_PASS         = "PASSWORD"
-DB_CON_BASE         = "DATABASE"
+DB_CON_HOST         = parser.get("DB", "host")
+DB_CON_USER         = parser.get("DB", "user")
+DB_CON_PASS         = parser.get("DB", "pass")
+DB_CON_BASE         = parser.get("DB", "base")
 # database insert template
-DB_INSERT_VALUE     = ("INSERT INTO sensordata (snid, sntime, dbtime, t_air, t_soil, h_air, h_soil, x_nt, x_vs, x_bat, x_art, x_rst, x_ic, x_adc, x_usart, label) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
+DB_INSERT_VALUE     = ("INSERT INTO sensordata_ftdca (snid, sntime, dbtime, t_air, t_soil, h_air, h_soil, soc, danger, safe, label) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
 
 # Email - SMTP
-EMAIL_RECEIVE       = 'receiver@mail.com'
-EMAIL_USER          = 'sender@mail.com'
-EMAIL_PASS          = '$MyPassWord$'
-EMAIL_SERVER        = 'smtp.mail.com'
+EMAIL_RECEIVE       = [parser.get("EMAIL", "receive")]
+EMAIL_USER          = parser.get("EMAIL", "user")
+EMAIL_PASS          = parser.get("EMAIL", "pass")
+EMAIL_SERVER        = parser.get("EMAIL", "server")
 EMAIL_PORT          = 587
 EMAIL_USE_SSL       = 0                     # otherwise use TLS
 
@@ -326,8 +330,8 @@ while (terminate != 1):
         # Payload length
         m_size = len(msg.data)
         
-        # Check message payload size (should be 18 bytes)
-        if m_size == 18:
+        # Check message payload size (should be 13 bytes)
+        if m_size == 13:
             ### Get use case data ###
             # 0..1  -> Sensor Node "timestamp"
             sntime    = int.from_bytes(msg.data[0:2], byteorder='little', signed=False)
@@ -338,95 +342,15 @@ while (terminate != 1):
             t_soil  = fixed16_to_float(int.from_bytes(msg.data[4:6],   byteorder='little', signed=False), 6)
             h_air   = fixed16_to_float(int.from_bytes(msg.data[6:8],   byteorder='little', signed=False), 6)
             h_soil  = fixed16_to_float(int.from_bytes(msg.data[8:10],  byteorder='little', signed=False), 6)
-            # -> Fault indicator
-            x_nt    = fixed8_to_float(msg.data[10], 6)
-            x_vs    = fixed8_to_float(msg.data[11], 6)
-            x_bat   = fixed8_to_float(msg.data[12], 6)
-            x_art   = fixed8_to_float(msg.data[13], 6)
-            x_rst   = fixed8_to_float(msg.data[14], 6)
-            x_ic    = fixed8_to_float(msg.data[15], 6)
-            x_adc   = fixed8_to_float(msg.data[16], 6)
-            x_usart = fixed8_to_float(msg.data[17], 6)
-            
-            ### Store last N sensor values ###
-            # Tair
-            if snid not in t_air_history:
-                # New node -> create empty list
-                t_air_history[snid] = []
-            t_air_history[snid].append(t_air)
-            if len(t_air_history[snid])>STDDEV_N:
-                t_air_history[snid].pop(0)
-            # Tsoil
-            if snid not in t_soil_history:
-                # New node -> create empty list
-                t_soil_history[snid] = []
-            t_soil_history[snid].append(t_soil)
-            if len(t_soil_history[snid])>STDDEV_N:
-                t_soil_history[snid].pop(0)
-            # Hair
-            if snid not in h_air_history:
-                # New node -> create empty list
-                h_air_history[snid] = []
-            h_air_history[snid].append(h_air)
-            if len(h_air_history[snid])>STDDEV_N:
-                h_air_history[snid].pop(0)
-            # Hsoil
-            if snid not in h_soil_history:
-                # New node -> create empty list
-                h_soil_history[snid] = []
-            h_soil_history[snid].append(h_soil)
-            if len(h_soil_history[snid])>STDDEV_N:
-                h_soil_history[snid].pop(0)
-            
-            ### Update dDCA indicators ###
-            # Store antigen
-            antigen = snid
-            # Calculate danger indicator
-            danger = min(1, (x_nt + x_vs + x_bat + x_art + x_rst + x_ic + x_adc + x_usart))
-            # Safe1 - T_air relative difference
-            safe1_mu = 0
-            for val in t_air_history[snid]:
-                safe1_mu = safe1_mu + val
-            safe1_mu = safe1_mu / len(t_air_history[snid])
-            safe1_dev = 0
-            for val in t_air_history[snid]:
-                safe1_dev = safe1_dev + ((val-safe1_mu)**2)
-            safe1_dev = safe1_dev / len(t_air_history[snid])
-            safe1_dev = math.sqrt(safe1_dev)
-            # Safe2 - T_soil relative difference
-            safe2_mu = 0
-            for val in t_soil_history[snid]:
-                safe2_mu = safe2_mu + val
-            safe2_mu = safe2_mu / len(t_soil_history[snid])
-            safe2_dev = 0
-            for val in t_soil_history[snid]:
-                safe2_dev = safe2_dev + ((val-safe2_mu)**2)
-            safe2_dev = safe2_dev / len(t_soil_history[snid])
-            safe2_dev = math.sqrt(safe2_dev)
-            # Safe3 - H_air relative difference
-            safe3_mu = 0
-            for val in h_air_history[snid]:
-                safe3_mu = safe3_mu + val
-            safe3_mu = safe3_mu / len(h_air_history[snid])
-            safe3_dev = 0
-            for val in h_air_history[snid]:
-                safe3_dev = safe3_dev + ((val-safe3_mu)**2)
-            safe3_dev = safe3_dev / len(h_air_history[snid])
-            safe3_dev = math.sqrt(safe3_dev)
-            # Safe4 - H_soil relative difference
-            safe4_mu = 0
-            for val in h_soil_history[snid]:
-                safe4_mu = safe4_mu + val
-            safe4_mu = safe4_mu / len(h_soil_history[snid])
-            safe4_dev = 0
-            for val in h_soil_history[snid]:
-                safe4_dev = safe4_dev + ((val-safe4_mu)**2)
-            safe4_dev = safe4_dev / len(h_soil_history[snid])
-            safe4_dev = math.sqrt(safe4_dev)
-            # Calculate final safe indicator
-            safe  = math.exp(-max(safe1_dev, safe2_dev, safe3_dev, safe4_dev)*SAFE_SENS)
+            # -> Battery SoC
+            soc     = msg.data[10]
+            # -> Indicator
+            danger  = fixed8_to_float(msg.data[11], 6)
+            safe    = fixed8_to_float(msg.data[12], 6)
             
             ### Dendritic cell update ###
+            # Store antigen
+            antigen = snid
             context = danger - safe
             # Create new DC
             dcs.append({
@@ -465,7 +389,7 @@ while (terminate != 1):
             if db_con.is_connected():
                 try:
                     # Try execute DB insert
-                    db_cur.execute(DB_INSERT_VALUE, (snid, sntime, tstamp, t_air, t_soil, h_air, h_soil, x_nt, x_vs, x_bat, x_art, x_rst, x_ic, x_adc, x_usart, label))
+                    db_cur.execute(DB_INSERT_VALUE, (snid, sntime, tstamp, t_air, t_soil, h_air, h_soil, soc, danger, safe, label))
                     # Commit data to the DB
                     db_con.commit()
                 except Exception as e:
@@ -544,7 +468,7 @@ while (terminate != 1):
                         # Insert data into DB
                         try:
                             # Try execute DB insert
-                            db_cur.execute(DB_INSERT_VALUE, (snid, sntime, tstamp, m_type, m_value, sreg, ""))
+                            db_cur.execute(DB_INSERT_VALUE, (snid, sntime, tstamp, t_air, t_soil, h_air, h_soil, soc, danger, safe, label))
                             # Commit data to the DB
                             db_con.commit()
                         except Exception as e:
